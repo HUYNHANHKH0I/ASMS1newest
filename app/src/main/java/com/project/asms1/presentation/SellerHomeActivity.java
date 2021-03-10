787f3df935fc496a4182db989ee6ece3262cce9e
An <Anptse140111@fpt.edu.vn>
2021-03-10 16:52:28 +0700

    dkm thg khoi v2

--- app/src/main/java/com/project/asms1/presentation/SellerHomeActivity.java
+++ app/src/main/java/com/project/asms1/presentation/SellerHomeActivity.java
@@ -1,15 +1,23 @@
 package com.project.asms1.presentation;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.project.asms1.R;
+import com.project.asms1.presentation.ui.home.HomeFragment;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+
+import java.io.BufferedReader;
 
 public class SellerHomeActivity extends AppCompatActivity {
 
@@ -21,7 +29,7 @@ public class SellerHomeActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_profile)
+                R.id.navigation_home, R.id.navigation_profile, R.id.navigation_store)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
 //          No Action bar nen ko set ActionbarNav
--- app/src/main/java/com/project/asms1/presentation/ui/home/HomeFragment.java
+++ app/src/main/java/com/project/asms1/presentation/ui/home/HomeFragment.java
@@ -1,10 +1,11 @@
 package com.project.asms1.presentation.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,6 +14,7 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.project.asms1.R;
+import com.project.asms1.presentation.OrderListActivity;
 
 public class HomeFragment extends Fragment {
 
@@ -23,11 +25,19 @@ public class HomeFragment extends Fragment {
         homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-//        final TextView textView = root.findViewById(R.id.text_home);
+
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
 
+            }
+        });
+        ImageButton btn_Order = root.findViewById(R.id.btn_Order);
+        btn_Order.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), OrderListActivity.class);
+                startActivity(intent);
             }
         });
         return root;
