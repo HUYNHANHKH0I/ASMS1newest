cd4c1519f07b4e471ab6bb1110a3b5da4e6bdbea
huynhanhkhoi <khoihase140138@fpt.edu.vn>
2021-03-21 00:37:30 +0700

    23.0 newest

--- app/src/main/java/com/project/asms1/presentation/PostDetailActivity.java
+++ app/src/main/java/com/project/asms1/presentation/PostDetailActivity.java
@@ -19,12 +19,14 @@ public class PostDetailActivity extends AppCompatActivity {
         TextView viewPostID = (TextView) findViewById(R.id.txt_post_ID_post_detail);
         TextView viewPostDate = (TextView) findViewById(R.id.txt_post_date_post_detail);
         TextView viewPostContent = (TextView) findViewById(R.id.txt_post_content_post_detail);
+        TextView viewPostTotalOrder = (TextView) findViewById(R.id.txt_post_total_order);
         Intent intent = getIntent();
         Post post = (Post) intent.getSerializableExtra("post");
 
         viewPostID.setText(post.getID());
         viewPostDate.setText(post.getTime().toString());
         viewPostContent.setText(post.getContent());
+        viewPostTotalOrder.setText(String.valueOf(post.getTotalOrder()));
     }
 
     public void clickToGoBack(View view) {
